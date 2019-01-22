822f0c69561201f68e04b8fc2b1474c67ad28912
XinZouS <sanking.saphira@foxmail.com>
2019-01-22 10:10:46 -0500

    ship module and moduleTreeNode

--- wowsAssistant/Models/ShipInfo.swift
+++ wowsAssistant/Models/ShipInfo.swift
@@ -37,12 +37,29 @@ enum ShipInfoKeyInDB: String {
     case is_special = "is_special"
     case name = "name"
 
+    // ShipModule
+    case engine = "engine"
+    case torpedo_bomber = "torpedo_bomber"
+    case fighter = "fighter"
+    case hull = "hull"
+    case artillery = "artillery"
+    case torpedoes = "torpedoes"
+    case fire_control = "fire_control"
+    case flight_control = "flight_control"
+    case dive_bomber = "dive_bomber"
+
+    //SHip Module Tree Node
+    case next_modules = "next_modules"
+    case is_default = "is_default"
+    case price_xp = "price_xp"
+    case module_id = "module_id"
+    case module_id_str = "module_id_str"
 }
 
 class ShipInfo: Unboxable {
     var description:    String?
     var ship_id:        Int = 0
-    var ship_id_str:    String?
+    var ship_id_str:    String = ""
     var has_demo_profile: Bool?
     var images:     [String:String]? // keys: "small", "medium", "large", "contour"
     var modules:    ShipModule?
@@ -61,9 +78,9 @@ class ShipInfo: Unboxable {
     var name:       String?
     
     required init(unboxer: Unboxer) {
-        self.description =  (try? unboxer.unbox(key: ShipInfoKeyInDB.description.rawValue))
-        self.ship_id =      (try? unboxer.unbox(key: ShipInfoKeyInDB.ship_id.rawValue)) ?? 0
-        self.ship_id_str =  try? unboxer.unbox(key: ShipInfoKeyInDB.ship_id_str.rawValue)
+        self.description = (try? unboxer.unbox(key: ShipInfoKeyInDB.description.rawValue))
+        self.ship_id =     (try? unboxer.unbox(key: ShipInfoKeyInDB.ship_id.rawValue)) ?? 0
+        self.ship_id_str = (try? unboxer.unbox(key: ShipInfoKeyInDB.ship_id_str.rawValue)) ?? "0"
         self.has_demo_profile = try? unboxer.unbox(key: ShipInfoKeyInDB.has_demo_profile.rawValue)
         self.images =       try? unboxer.unbox(key: ShipInfoKeyInDB.images.rawValue)
         self.modules =      try? unboxer.unbox(key: ShipInfoKeyInDB.modules.rawValue)
@@ -84,28 +101,52 @@ class ShipInfo: Unboxable {
 }
 
 
-struct ShipModule {
-    let engine:         [Int]
-    let torpedo_bomber: [Int]
-    let fighter:        [Int]
-    let hull:           [Int]
-    let artillery:      [Int]
-    let torpedoes:      [Int]
-    let fire_control:   [Int]
-    let flight_control: [Int]
-    let dive_bomber:    [Int]
+class ShipModule: Unboxable {
+    var engine:         [Int]?
+    var torpedo_bomber: [Int]?
+    var fighter:        [Int]?
+    var hull:           [Int]?
+    var artillery:      [Int]?
+    var torpedoes:      [Int]?
+    var fire_control:   [Int]?
+    var flight_control: [Int]?
+    var dive_bomber:    [Int]?
+    
+    required init(unboxer: Unboxer) throws {
+        engine =        try? unboxer.unbox(key: ShipInfoKeyInDB.engine.rawValue)
+        torpedo_bomber = try? unboxer.unbox(key: ShipInfoKeyInDB.torpedo_bomber.rawValue)
+        fighter =       try? unboxer.unbox(key: ShipInfoKeyInDB.fighter.rawValue)
+        hull =          try? unboxer.unbox(key: ShipInfoKeyInDB.hull.rawValue)
+        artillery =     try? unboxer.unbox(key: ShipInfoKeyInDB.artillery.rawValue)
+        torpedoes =     try? unboxer.unbox(key: ShipInfoKeyInDB.torpedoes.rawValue)
+        fire_control =  try? unboxer.unbox(key: ShipInfoKeyInDB.fire_control.rawValue)
+        flight_control = try? unboxer.unbox(key: ShipInfoKeyInDB.flight_control.rawValue)
+        dive_bomber =   try? unboxer.unbox(key: ShipInfoKeyInDB.dive_bomber.rawValue)
+    }
 }
 
-struct ShipModuleTreeNode {
-    let name: String
-    let next_modules: String
-    let is_default: Bool
-    let price_xp: Int
-    let price_credit: Int
-    let next_ships: String
-    let module_id: String
-    let type: String
-    let module_id_str: String
+class ShipModuleTreeNode: Unboxable {
+    var module_id: Int = 0
+    var module_id_str: String = "0"
+    var name: String = ""
+    var next_modules: String?
+    var is_default: Bool?
+    var price_xp: Int?
+    var price_credit: Int?
+    var next_ships: String?
+    var type: String?
+    
+    required init(unboxer: Unboxer) throws {
+        module_id =     (try? unboxer.unbox(key: ShipInfoKeyInDB.module_id.rawValue)) ?? 0
+        module_id_str = (try? unboxer.unbox(key: ShipInfoKeyInDB.module_id_str.rawValue)) ?? "0"
+        name =          (try? unboxer.unbox(key: ShipInfoKeyInDB.name.rawValue)) ?? ""
+        next_modules =  try? unboxer.unbox(key: ShipInfoKeyInDB.next_modules.rawValue)
+        is_default =    try? unboxer.unbox(key: ShipInfoKeyInDB.is_default.rawValue)
+        price_xp =      try? unboxer.unbox(key: ShipInfoKeyInDB.price_xp.rawValue)
+        price_credit =  try? unboxer.unbox(key: ShipInfoKeyInDB.price_credit.rawValue)
+        next_ships =    try? unboxer.unbox(key: ShipInfoKeyInDB.next_ships.rawValue)
+        type =          try? unboxer.unbox(key: ShipInfoKeyInDB.type.rawValue)
+    }
 }
 
 
