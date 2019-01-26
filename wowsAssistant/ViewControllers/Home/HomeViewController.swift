bd1917be5bac2a5426df3069358c56363988e623
XinZouS <sanking.saphira@foxmail.com>
2019-01-26 18:50:04 -0500

    add webview

--- wowsAssistant/ViewControllers/Home/HomeViewController.swift
+++ wowsAssistant/ViewControllers/Home/HomeViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
-class HomeViewController: BasicViewController {
+class HomeViewController: BasicViewController, WKNavigationDelegate {
     
     // https://worldofwarships.asia/zh-tw/news/
     // https://worldofwarships.com/en/news/
     let wowsNewsUrl = "https://worldofwarships.com/en/news/"
     
+    let webView = WKWebView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupWebView()
+        loadWebpageFromUrl()
+    }
+    
+    private func setupWebView() {
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        webView.addConstraint(view.leftAnchor, view.topAnchor, view.rightAnchor, view.bottomAnchor)
+    }
+    
+    private func loadWebpageFromUrl() {
+        guard let url = URL(string: wowsNewsUrl) else { return }
+        DispatchQueue.main.async {
+            self.webView.load(URLRequest(url: url))
+            // TODO: use my activity indicator:
+//            self.activityIndicator?.isHidden = false
+//            self.activityIndicator?.animate()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        activityIndicator?.isHidden = true
+//        activityIndicator?.stop()
+    }
     
 }
